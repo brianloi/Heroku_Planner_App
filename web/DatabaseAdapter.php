8a80bcc31cdf4b3f7ac4be70dec2d12761f81849
brianloi <i3rianloi@gmail.com>
2018-06-27 19:16:32 -0700

    pls

--- web/DatabaseAdapter.php
+++ web/DatabaseAdapter.php
@@ -18,7 +18,7 @@ class DatabaseAdaptor {
 		    $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
 		    // set the PDO error mode to exception
 		    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		    echo "Connected successfully";
+		    //echo "Connected successfully";
 		    }
 		catch(PDOException $e)
 		    {
